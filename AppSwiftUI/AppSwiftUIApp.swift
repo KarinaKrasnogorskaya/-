a7391d7d9433b9edc9f831805a4eef0d1b0b063c
KarinaKR00 <kr_karina00@mail.ru>
2023-03-08 19:31:35 +0300

    Initial Commit

---
+++ AppSwiftUI/AppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwiftUIApp.swift
+//  AppSwiftUI
+//
+//  Created by Карина on 08.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
